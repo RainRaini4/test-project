8432db269834c1d10d299bd8ae702137b8523d1b
Rain <mihail.qweasdzxc1@gmail.com>
2022-10-13 18:27:33 +0300

    Сделал 4 слайдера

--- src/app/components/sliders/sliders.component.ts
+++ src/app/components/sliders/sliders.component.ts
@@ -1,21 +1,30 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatSliderChange} from "@angular/material/slider";
 import {Equalizer} from "./equalizer";
+
+interface Sliders {
+  val1: number,
+  val2: number,
+  val3: number,
+  val4: number
+}
 
 @Component({
   selector: 'app-sliders',
   templateUrl: './sliders.component.html',
-  styleUrls: ['./sliders.component.scss']
+  styleUrls: ['./sliders.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SlidersComponent {
 
-  public sliders = {
+  public sliders : Sliders = {
     val1: 0,
     val2: 0,
     val3: 0,
+    val4: 0
   }
-  public min = -100
-  public max = 100
+  public min : number = -100
+  public max : number = 100
 
   private equalizer: Equalizer;
 
@@ -23,11 +32,11 @@ export class SlidersComponent {
     this.equalizer = new Equalizer(
       this.min,
       this.max,
-      [ this.sliders.val1, this.sliders.val2, this.sliders.val3 ]
+      [ this.sliders.val1, this.sliders.val2, this.sliders.val3, this.sliders.val4 ]
     );
   }
 
-  public getSum () : number {return this.sliders.val1 + this.sliders.val2 + this.sliders.val3}
+  public getSum () : number {return this.sliders.val1 + this.sliders.val2 + this.sliders.val3 + this.sliders.val4}
 
   public onInputChange(event: MatSliderChange, sliderIndex: number) {
     if (event.value === null) return;
@@ -35,6 +44,7 @@ export class SlidersComponent {
     this.sliders.val1 = results[ 0 ];
     this.sliders.val2 = results[ 1 ];
     this.sliders.val3 = results[ 2 ];
+    this.sliders.val4 = results[ 3 ];
   }
 
 }
